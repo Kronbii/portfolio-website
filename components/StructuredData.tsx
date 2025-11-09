17b32b7749372cfcd4fd6fc48c229ae1d69646d2
kronbii <145912020+Kronbii@users.noreply.github.com>
2025-11-09 13:03:42 +0200

    edited schema

--- components/StructuredData.tsx
+++ components/StructuredData.tsx
@@ -5,6 +5,7 @@ export default function StructuredData() {
     name: 'Rami Kronbi',
     jobTitle: 'AI & Computer Vision Engineer',
     url: 'https://ramikronbi.com',
+    image: 'https://ramikronbi.com/icon-512.png',
     sameAs: [
       'https://github.com/Kronbii',
       'https://www.linkedin.com/in/rami-kronbi/'
@@ -27,28 +28,37 @@ export default function StructuredData() {
     '@context': 'https://schema.org',
     '@type': 'WebSite',
     name: 'Rami Kronbi',
-    alternateName: 'Rami Kronbi Portfolio',
+    alternateName: 'Rami Kronbi',
     url: 'https://ramikronbi.com',
+    publisher: {
+      '@type': 'Person',
+      name: 'Rami Kronbi',
+      url: 'https://ramikronbi.com'
+    },
+    inLanguage: 'en-US',
     potentialAction: {
       '@type': 'SearchAction',
       target: 'https://ramikronbi.com/?q={search_term_string}',
       'query-input': 'required name=search_term_string'
-    },
-    author: {
-      '@type': 'Person',
-      name: 'Rami Kronbi'
     }
   }
 
   return (
     <>
+      {/* Person Schema */}
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(personSchema)
+        }}
       />
+
+      {/* Website Schema */}
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(websiteSchema)
+        }}
       />
     </>
   )
