627542ce2ea96d2d72628a42d0427273550c720c
kronbii <145912020+Kronbii@users.noreply.github.com>
2026-01-31 01:20:56 +0200

    refinement

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
       <Hero />
       <Community />
       <Experience />
-      <Blog />
       <Projects />
+      <Blog />
       <Certifications />
       <Services />
       <Contact />
